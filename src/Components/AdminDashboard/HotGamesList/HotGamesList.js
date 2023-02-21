a7cd4877b0454581ac5c2dbcb62069edcc9cbdf4
muhammad farhan <54625714+malekith-the-coder@users.noreply.github.com>
2023-02-21 04:51:35 -0500

    pending changes completed

--- src/Components/AdminDashboard/HotGamesList/HotGamesList.js
+++ src/Components/AdminDashboard/HotGamesList/HotGamesList.js
@@ -3,6 +3,15 @@ import React, { useState } from 'react'
 function HotGamesList() {
 
     const [hot, setHot] = useState(false)
+    const [hotOne, setHotOne] = useState(false)
+    const [hotTwo, setHotTwo] = useState(false)
+    const [hotThree, setHotThree] = useState(false)
+    const [hotFour, setHotFour] = useState(false)
+    const [hotFive, setHotFive] = useState(false)
+    const [hotSix, setHotSix] = useState(false)
+    const [hotSeven, setHotSeven] = useState(false)
+    const [hotEight, setHotEight] = useState(false)
+    const [hotNine, setHotNine] = useState(false)
 
     return (
         <div className='row'>
@@ -31,63 +40,63 @@ function HotGamesList() {
                             <td className='date_of_bet'>Hash Dice</td>
                             <td className='bet_amount'>50000</td>
                             <td className='total_bets'><div>786</div></td>
-                            <td><img src="/fire.png" alt="" /></td>
+                            <td><span onClick={() =>setHotOne(hotOne => !hotOne)}>{hotOne ? <img src="/hot-fire.png" alt="" /> : <img src="/fire.png" alt="" />}</span></td>
                             <hr />
                         </tr>
                         <tr>
                             <td className='date_of_bet'>Hash Dice</td>
                             <td className='bet_amount'>50000</td>
                             <td className='total_bets'><div>786</div></td>
-                            <td><img src="/hot-fire.png" alt="" /></td>
+                            <td><span onClick={() =>setHotTwo(hotTwo => !hotTwo)}>{hotTwo ? <img src="/hot-fire.png" alt="" /> : <img src="/fire.png" alt="" />}</span></td>
                             <hr />
                         </tr>
                         <tr>
                             <td className='date_of_bet'>Hash Dice</td>
                             <td className='bet_amount'>50000</td>
                             <td className='total_bets'><div>786</div></td>
-                            <td><img src="/fire.png" alt="" /></td>
+                            <td><span onClick={() =>setHotThree(hotThree => !hotThree)}>{hotThree ? <img src="/hot-fire.png" alt="" /> : <img src="/fire.png" alt="" />}</span></td>
                             <hr />
                         </tr>
                         <tr>
                             <td className='date_of_bet'>Hash Dice</td>
                             <td className='bet_amount'>50000</td>
                             <td className='total_bets'><div>786</div></td>
-                            <td><img src="/hot-fire.png" alt="" /></td>
+                            <td><span onClick={() =>setHotFour(hotFour => !hotFour)}>{hotFour ? <img src="/hot-fire.png" alt="" /> : <img src="/fire.png" alt="" />}</span></td>
                             <hr />
                         </tr>
                         <tr>
                             <td className='date_of_bet'>Hash Dice</td>
                             <td className='bet_amount'>50000</td>
                             <td className='total_bets'><div>786</div></td>
-                            <td><img src="/fire.png" alt="" /></td>
+                            <td><span onClick={() =>setHotFive(hotFive => !hotFive)}>{hotFive ? <img src="/hot-fire.png" alt="" /> : <img src="/fire.png" alt="" />}</span></td>
                             <hr />
                         </tr>
                         <tr>
                             <td className='date_of_bet'>Hash Dice</td>
                             <td className='bet_amount'>50000</td>
                             <td className='total_bets'><div>786</div></td>
-                            <td><img src="/hot-fire.png" alt="" /></td>
+                            <td><span onClick={() =>setHotSix(hotSix => !hotSix)}>{hotSix ? <img src="/hot-fire.png" alt="" /> : <img src="/fire.png" alt="" />}</span></td>
                             <hr />
                         </tr>
                         <tr>
                             <td className='date_of_bet'>Hash Dice</td>
                             <td className='bet_amount'>50000</td>
                             <td className='total_bets'><div>786</div></td>
-                            <td><img src="/fire.png" alt="" /></td>
+                            <td><span onClick={() =>setHotSeven(hotSeven => !hotSeven)}>{hotSeven ? <img src="/hot-fire.png" alt="" /> : <img src="/fire.png" alt="" />}</span></td>
                             <hr />
                         </tr>
                         <tr>
                             <td className='date_of_bet'>Hash Dice</td>
                             <td className='bet_amount'>50000</td>
                             <td className='total_bets'><div>786</div></td>
-                            <td><img src="/hot-fire.png" alt="" /></td>
+                            <td><span onClick={() =>setHotEight(hotEight => !hotEight)}>{hotEight ? <img src="/hot-fire.png" alt="" /> : <img src="/fire.png" alt="" />}</span></td>
                             <hr />
                         </tr>
                         <tr>
                             <td className='date_of_bet'>Hash Dice</td>
                             <td className='bet_amount'>50000</td>
                             <td className='total_bets'><div>786</div></td>
-                            <td><img src="/fire.png" alt="" /></td>
+                            <td><span onClick={() =>setHotNine(hotNine => !hotNine)}>{hotNine ? <img src="/hot-fire.png" alt="" /> : <img src="/fire.png" alt="" />}</span></td>
                             <hr />
                         </tr>
                     </tbody>
